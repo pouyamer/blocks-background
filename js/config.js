b6f28afce03efed8eba0112cb241e0337aa5f38a
Pouya Moazzezi <pmoazzezi@gmail.com>
2022-06-25 00:21:56 +0430

    Added more clarity, and added hue changing option.

--- js/config.js
+++ js/config.js
@@ -1,22 +1,51 @@
-const config = {
-  canvasSize: {
-    width: innerWidth,
-    height: innerHeight
-  },
+let config = {
+  canvas: { size: { width: innerWidth, height: innerHeight } },
   square: {
     light: {
-      max: 30,
-      min: 17,
+      // if the l value in hsl
+      //  - true: uses the range
+      //  - false: uses the value
+      isRanged: true,
+      range: {
+        // Max and Min value of light is [0 - 100]
+        min: 27,
+        max: 40
+      },
+      // if !isRanged then it gets the Value [0 - 100]
+      value: 40,
+      //------------------------------
       // rate of decrease / increase
       step: {
-        increase: 1,
-        decrease: 4
+        increase: 2,
+        decrease: 5
       },
-      // changes the light value on random [0 - incOrDec]
+      // if true changes the light value on random [0 - incOrDec]
       randomlyChange: true
     },
 
-    size: 17, // Try above 15 for better performence
+    hue: {
+      // if the h value in hsl
+      //  - true: uses the range
+      //  - false: uses the value
+      isRanged: true,
+      range: {
+        // Max and Min value of hue is [0 - 360]
+        min: 240,
+        max: 230
+      },
+      // if !isRanged then it gets the Value [0 - 360]
+      value: 20,
+      //------------------------------
+      // rate of decrease / increase
+      step: {
+        increase: 3,
+        decrease: 5
+      },
+      // if true changes the hue value on random [0 - incOrDec]
+      randomlyChange: true
+    },
+
+    size: 13, // Try above 15 for better performence
     fillColor: {
       h: 340,
       s: 100,
@@ -31,3 +60,5 @@ const config = {
     }
   }
 }
+
+// ADD: Error Handling (light.min > light.max)
