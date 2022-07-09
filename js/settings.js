c119dbe85222c96aa776d2648890412064a8d250
Pouya Moazzezi <pmoazzezi@gmail.com>
2022-07-09 23:42:05 +0430

    Added Hue Settings.

--- js/settings.js
+++ js/settings.js
@@ -1,80 +1,15 @@
 const hamburgerButton = document.querySelector(".hamburger-container")
 const settingsElement = document.querySelector(".settings")
-const [barTop, barMiddle, barBottom] = hamburgerButton.querySelectorAll(".bar")
+const [hueBarContainer] = document.querySelectorAll(".bar-container")
+const [hueBar] = document.querySelectorAll(".bar")
+const [barTop, barMiddle, barBottom] =
+  hamburgerButton.querySelectorAll(".hamburger-bar")
 
-const hslToRgb = hslColor => {
-  const h = hslColor.h
-  const s = hslColor.s / 100
-  const l = hslColor.l / 100
+let hueBarMaxIsChanging = document.querySelector(".hue-bar-max-is-changing")
+let hueBarMinIsChanging = false
 
-  const C = (1 - Math.abs(2 * l - 1)) * s
-  const X = C * (1 - Math.abs(((h / 60) % 2) - 1))
-  const m = l - C / 2
-
-  const get_rPrime_gPrime_bPrime = (h, C, X) => {
-    if (h >= 0 && h < 60) return [C, X, 0]
-    if (h < 120) return [X, C, 0]
-    if (h < 180) return [0, C, X]
-    if (h < 240) return [0, X, C]
-    if (h < 300) return [X, 0, C]
-    return [C, 0, X]
-  }
-
-  const [rPrime, gPrime, bPrime] = get_rPrime_gPrime_bPrime(h, C, X)
-
-  return {
-    r: Math.round((rPrime + m) * 255),
-    g: Math.round((gPrime + m) * 255),
-    b: Math.round((bPrime + m) * 255)
-  }
-}
-
-const rgbToHsl = rgbColor => {
-  const { r, g, b } = rgbColor
-
-  const rPrime = r / 255
-  const gPrime = g / 255
-  const bPrime = b / 255
-
-  const cMax = Math.max(rPrime, gPrime, bPrime)
-  const cMin = Math.min(rPrime, gPrime, bPrime)
-
-  const delta = cMax - cMin
-
-  const calculateHue = (rPrime, gPrime, bPrime, delta) => {
-    if (delta === 0) return 0
-    if (rPrime === cMax) return 60 * (((gPrime - bPrime) / delta) % 6)
-    if (gPrime === cMax) return 60 * ((bPrime - rPrime) / delta + 2)
-    if (bPrime === cMax) return 60 * ((rPrime - gPrime) / delta + 4)
-  }
-
-  const calculateSaturation = (delta, l) => {
-    return delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
-  }
-
-  const hue =
-    calculateHue(rPrime, gPrime, bPrime, delta) < 0
-      ? 360 + calculateHue(rPrime, gPrime, bPrime, delta)
-      : calculateHue(rPrime, gPrime, bPrime, delta)
-  const light = (cMax + cMin) / 2
-  const saturation = calculateSaturation(delta, light)
-
-  return {
-    h: hue,
-    s: saturation * 100,
-    l: light * 100,
-    a: 1
-  }
-}
-
-const invertColor = hslColor => {
-  const rgbColor = hslToRgb(hslColor)
-  const { r, g, b } = rgbColor
-  return rgbToHsl({
-    r: 255 - r,
-    g: 255 - g,
-    b: 255 - b
-  })
+let unsavedConfig = {
+  ...config
 }
 
 const updateHamburgerColor = (topColor, bottomColor) => {
@@ -121,10 +56,70 @@ const getTopAndBottomColor = config => {
   return [topColor, bottomColor]
 }
 
+const updateBar = (barContainer, minValue, maxValue) => {
+  // Updating color of the bar
+  const bar = barContainer.querySelector(".bar")
+
+  // setting the grid template columns
+  bar.style.gridTemplateColumns = `${minValue}fr ${maxValue - minValue}fr ${
+    360 - maxValue
+  }fr`
+
+  // setting the color of the bar
+  const gradientText = `linear-gradient( to right, ${Array(20)
+    .fill()
+    .map((_, i) =>
+      hslStringify({
+        h:
+          barContainer === hueBarContainer
+            ? minValue + (i * (maxValue - minValue)) / 20
+            : 30,
+        s:
+          // barContainer === saturationBarContainer
+          false ? minValue + (i * (maxValue - minValue)) / 20 : 80,
+        l:
+          // barContainer === lightBarContainer
+          false ? minValue + (i * (maxValue - minValue)) / 20 : 60,
+        a: 1
+      })
+    )
+    .join(", ")} )`
+
+  bar.querySelector(".selected").style.backgroundImage = gradientText
+
+  // setting the min and max hue values in the inputs
+  const min = barContainer.querySelector(".min")
+  const max = barContainer.querySelector(".max")
+
+  min.value = minValue
+  max.value = maxValue
+}
+
 hamburgerButton.addEventListener("click", () => {
   settingsElement.classList.toggle("settings-open")
   hamburgerButton.classList.toggle("settings-open")
 })
 
+hueBar.addEventListener("mousemove", e => {
+  // get the clicked position
+  const x = e.offsetX
+  // get the relative position (0 - 1)
+  const relativeX = x / hueBar.offsetWidth
+  const chosenHue = relativeX * 360
+
+  console.log(chosenHue)
+  if (hueBarMinIsChanging) {
+    const minHue = chosenHue
+    const maxHue = 360
+
+    updateBar(hueBarContainer, minHue, maxHue)
+  }
+})
+
 // Initial Run
 updateHamburgerColor(...getTopAndBottomColor(config))
+updateBar(
+  hueBarContainer,
+  unsavedConfig.square.hue.range.min,
+  unsavedConfig.square.hue.range.max
+)
